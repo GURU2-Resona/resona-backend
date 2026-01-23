10fc735975f51b98dda848fb4dfac68913a3965a
yangjiae12 <yangjiae12@gmail.com>
2026-01-24 01:17:06 +0900

    feat: 회원 프로필 조회 서비스 로직 구현 #13

--- src/main/java/com/resona/domain/member/converter/MemberConverter.java
+++ src/main/java/com/resona/domain/member/converter/MemberConverter.java
@@ -1,4 +1,15 @@
 package com.resona.domain.member.converter;
 
+import com.resona.domain.member.dto.MemberResDto;
+import com.resona.domain.member.entity.Member;
+
 public class MemberConverter {
+
+    public static MemberResDto.Profile toProfileResDto(Member member) {
+        return MemberResDto.Profile.builder()
+                .id(member.getId())
+                .nickname(member.getNickname())
+                .profileImage(member.getProfileImage())
+                .build();
+    }
 }
