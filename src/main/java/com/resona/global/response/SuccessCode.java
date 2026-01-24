76ca0f499470dc72ba9ff4b4c9b0ed4a81a5a601
minkyung <HaMming.bright@gmail.com>
2026-01-24 18:52:14 +0900

    feat: 스크랩 저장과 취소 controller 생성 및 성공 코드 작성

--- src/main/java/com/resona/global/response/SuccessCode.java
+++ src/main/java/com/resona/global/response/SuccessCode.java
@@ -14,7 +14,9 @@ public enum SuccessCode implements BaseCode {
   MEMBER_PROFILE_GET_OK(HttpStatus.OK, "MEMBER2001", "회원 프로필 조회에 성공하였습니다."),
 
   // post
-  POST_SAVE_OK(HttpStatus.CREATED, "POST2001", "추천글이 성공적으로 작성되었습니다.");
+  POST_SAVE_OK(HttpStatus.CREATED, "POST2001", "추천글이 성공적으로 작성되었습니다."),
+  POST_SCRAP_OK(HttpStatus.OK, "POST2002", "추천글이 보관함에 저장되었습니다."),
+  POST_UNSCRAP_OK(HttpStatus.OK, "POST2003", "추천글이 보관함에서 삭제되었습니다.");
 
   private final HttpStatus status;
   private final String code;
