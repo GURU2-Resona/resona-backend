d6d43262b5f4b4c4b8b01aa349ed9a53218abd5d
minkyung <HaMming.bright@gmail.com>
2026-01-24 21:34:20 +0900

    feat: enum에 따라 entity 변경

--- src/main/java/com/resona/domain/post/entity/Post.java
+++ src/main/java/com/resona/domain/post/entity/Post.java
@@ -1,6 +1,8 @@
 package com.resona.domain.post.entity;
 
 import com.resona.domain.member.entity.Member;
+import com.resona.domain.post.entity.enums.Category;
+import com.resona.domain.post.entity.enums.Scene;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -12,34 +14,47 @@ import lombok.*;
 @Table(name = "post")
 public class Post {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(name = "title", nullable = false, length = 50)
-  private String title;
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
 
-  @Column(name = "content", nullable = false)
-  private String content;
+    @Column(name = "content", nullable = false)
+    private String content;
 
-  @Column(name = "song_title", nullable = false)
-  private String songTitle;
+    @Column(name = "song_title", nullable = false)
+    private String songTitle;
 
-  @Column(name = "singer", nullable = false, length = 50)
-  private String singer;
+    @Column(name = "singer", nullable = false, length = 50)
+    private String singer;
 
-  @Column(name = "song_url", nullable = false)
-  private String songUrl;
+    @Column(name = "song_url", nullable = false)
+    private String songUrl;
 
-  // 앨범 커버(썸네일) 이미지
-  @Column(name = "album_image")
-  private String albumImage;
+    @Column(name = "album_image")
+    private String albumImage;
 
-  @Column(name = "saved")
-  @Builder.Default
-  private Boolean saved = false;
+    @Column(name = "saved")
+    @Builder.Default
+    private Boolean saved = false;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "member_id", nullable = false)
-  private Member member;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category", nullable = false)
+    private Category category;
+
+    @Column(name = "custom_category")
+    private String customCategory;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "scene", nullable = false)
+    private Scene scene;
+
+    @Column(name = "custom_scene")
+    private String customScene;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 }
