640e90749333d0179cd56a1a6ba8ba02980f7b97
minkyung <HaMming.bright@gmail.com>
2026-01-25 00:15:24 +0900

    feat: 필터링 시에 선택한 카테고리 이름과 상황 이름 추가 반환

--- src/main/java/com/resona/domain/post/dto/PostListResponse.java
+++ src/main/java/com/resona/domain/post/dto/PostListResponse.java
@@ -1,6 +1,8 @@
 package com.resona.domain.post.dto;
 
 import com.resona.domain.post.entity.Post;
+import com.resona.domain.post.entity.enums.Category;
+import com.resona.domain.post.entity.enums.Scene;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,8 +18,18 @@ public class PostListResponse {
     private String albumImage;         // 앨범 커버 이미지
     private String writerProfileImage; // 작성자 프로필 이미지
     private String writerNickname;     // 작성자 닉네임
+    private String categoryName;
+    private String sceneName;
 
     public static PostListResponse of(Post post) {
+        //기타일 때 처리하는 로직
+        String displayCategory = (post.getCategory() == Category.OTHER)
+                ? post.getCustomCategory()
+                : post.getCategory().getDescription();
+
+        String displayScene = (post.getScene() == Scene.OTHER)
+                ? post.getCustomScene()
+                : post.getScene().getDescription();
         return PostListResponse.builder()
                 .postId(post.getId())
                 .title(post.getTitle())
@@ -25,6 +37,8 @@ public class PostListResponse {
                 .albumImage(post.getAlbumImage())
                 .writerProfileImage(post.getMember().getProfileImage())
                 .writerNickname(post.getMember().getNickname())
+                .categoryName(displayCategory)
+                .sceneName(displayScene)
                 .build();
     }
 }
