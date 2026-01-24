5f3ba46933266c56d49259ca76898c38eb59dc7c
seoyoon lee <leeseoyoon01@gmail.com>
2026-01-24 13:44:24 +0900

    style: merge 오류 수정

--- src/main/java/com/resona/global/response/SuccessCode.java
+++ src/main/java/com/resona/global/response/SuccessCode.java
@@ -27,7 +27,7 @@ public enum SuccessCode implements BaseCode {
     //user
     USER_UPDATE_OK(HttpStatus.OK, "USER2001", "사용자 정보가 수정되었습니다."),
     NICKNAME_SAVE_OK(HttpStatus.OK ,"MEMBER200", "닉네임이 저장되었습니다."),
-    MEMBER_PROFILE_GET_OK(HttpStatus.OK, "MEMBER2001", "회원 프로필 조회에 성공하였습니다.");
+    MEMBER_PROFILE_GET_OK(HttpStatus.OK, "MEMBER2001", "회원 프로필 조회에 성공하였습니다."),
     USER_LOGIN_OK(HttpStatus.OK ,"MEMBER200_2", "로그인에 성공했습니다..");
 
     private final HttpStatus status;
