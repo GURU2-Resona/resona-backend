90ded496b9182eaaa477fe8bce1764454964a857
seoyoon lee <leeseoyoon01@gmail.com>
2026-01-24 18:49:26 +0900

    fix: 성공응답 누락 수정

--- src/main/java/com/resona/global/response/SuccessCode.java
+++ src/main/java/com/resona/global/response/SuccessCode.java
@@ -28,7 +28,11 @@ public enum SuccessCode implements BaseCode {
     USER_UPDATE_OK(HttpStatus.OK, "USER2001", "사용자 정보가 수정되었습니다."),
     NICKNAME_SAVE_OK(HttpStatus.OK ,"MEMBER200", "닉네임이 저장되었습니다."),
     MEMBER_PROFILE_GET_OK(HttpStatus.OK, "MEMBER2001", "회원 프로필 조회에 성공하였습니다."),
-    USER_LOGIN_OK(HttpStatus.OK ,"MEMBER200_2", "로그인에 성공했습니다..");
+    USER_LOGIN_OK(HttpStatus.OK ,"MEMBER200_2", "로그인에 성공했습니다.."),
+
+
+    // post
+    POST_SAVE_OK(HttpStatus.CREATED, "POST2001", "추천글이 성공적으로 작성되었습니다.");
 
     private final HttpStatus status;
     private final String code;
