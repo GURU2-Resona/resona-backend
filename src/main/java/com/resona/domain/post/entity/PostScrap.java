be343ffa9301528dfd3c9a464cf59a53b5f89124
minkyung <HaMming.bright@gmail.com>
2026-01-24 18:44:36 +0900

    feat: postscrap Entity 생성

---
+++ src/main/java/com/resona/domain/post/entity/PostScrap.java
@@ -0,0 +1,34 @@
+package com.resona.domain.post.entity;
+
+import com.resona.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "post_scrap")
+public class PostScrap {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    // 생성 메서드
+    public static PostScrap createScrap(Member member, Post post) {
+        return PostScrap.builder()
+                .member(member)
+                .post(post)
+                .build();
+    }
+}
