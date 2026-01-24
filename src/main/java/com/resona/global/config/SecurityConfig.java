c22ec16ce177caa16db57ce7cd33879865111c43
seoyoon lee <leeseoyoon01@gmail.com>
2026-01-24 22:48:18 +0900

    feat: config 설정 #22

--- src/main/java/com/resona/global/config/SecurityConfig.java
+++ src/main/java/com/resona/global/config/SecurityConfig.java
@@ -1,31 +1,36 @@
 package com.resona.global.config;
 
+import com.resona.global.oAuth.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf()
-        .disable()
-        .authorizeHttpRequests(
-            auth ->
-                auth.requestMatchers(
-                        "/auth/kakao/**",
-                        "/swagger-ui/**",
-                        "/v3/api-docs/**",
-                        "/swagger-resources/**",
-                        "/members/**")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated());
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    return http.build();
-  }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/members/login/kakao",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+        )
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
+
+        return http.build();
+    }
 }
