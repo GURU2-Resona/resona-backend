20bec170849d8f5bab33d766275e50406d7fdb00
minkyung <HaMming.bright@gmail.com>
2026-01-24 23:54:17 +0900

    feat: 목록 dto response 생성

---
+++ src/main/java/com/resona/domain/post/dto/PostListResponse.java
@@ -0,0 +1,30 @@
+package com.resona.domain.post.dto;
+
+import com.resona.domain.post.entity.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class PostListResponse {
+
+    private Long postId;
+    private String title;              // 추천글 제목
+    private String songTitle;          // 노래 제목
+    private String albumImage;         // 앨범 커버 이미지
+    private String writerProfileImage; // 작성자 프로필 이미지
+    private String writerNickname;     // 작성자 닉네임
+
+    public static PostListResponse of(Post post) {
+        return PostListResponse.builder()
+                .postId(post.getId())
+                .title(post.getTitle())
+                .songTitle(post.getSongTitle())
+                .albumImage(post.getAlbumImage())
+                .writerProfileImage(post.getMember().getProfileImage())
+                .writerNickname(post.getMember().getNickname())
+                .build();
+    }
+}
