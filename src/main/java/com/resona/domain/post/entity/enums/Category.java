c1d88eedb4a4674b5f1be36556f38ddb2a05f9ab
minkyung <HaMming.bright@gmail.com>
2026-01-25 19:31:16 +0900

    feat: 카테고리 변경

--- src/main/java/com/resona/domain/post/entity/enums/Category.java
+++ src/main/java/com/resona/domain/post/entity/enums/Category.java
@@ -6,12 +6,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Category {
-  BALLAD("발라드/감성"),
-  DANCE("댄스/팝"),
-  HIPHOP("힙합/알앤비"),
-  INDIE("인디/포크"),
-  JAZZ("재즈/클래식"),
-  ROCK("락/밴드"),
+  ROMANCE("사랑/설렘"),
+  FAREWELL("이별/슬픔"),
+  COMFORT("위로/응원"),
+  ENERGY("신남/에너지"),
+  LONGING("그리움/추억"),
+  RELAX("일상/여유"),
   OTHER("기타");
 
   private final String description;
