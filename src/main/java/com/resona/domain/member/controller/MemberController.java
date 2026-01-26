95cf958b26b60e80e2bb6662b482e92fb964c867
seoyoon lee <leeseoyoon01@gmail.com>
2026-01-26 22:24:03 +0900

    refactor: 경로 수정

--- src/main/java/com/resona/domain/member/controller/MemberController.java
+++ src/main/java/com/resona/domain/member/controller/MemberController.java
@@ -46,7 +46,7 @@ public class MemberController implements MemberControllerDocs {
     return ApiResponse.onSuccess(SuccessCode.MEMBER_PROFILE_IMAGE_OK, response);
   }
 
-  @GetMapping("/members/profile/me")
+  @GetMapping("/profile/me")
   public ApiResponse<MemberResDto.Profile> getMyProfile(
       @RequestHeader("Authorization") String token) {
     MemberResDto.Profile response = memberService.getMyProfile(token);
